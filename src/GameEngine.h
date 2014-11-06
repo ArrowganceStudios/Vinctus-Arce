9f3eac5a5ad46a626e076053cbb48dbd77a2e08c
Sand3rfulx <sand3r@interia.eu>
2014-11-06 17:59:40 +0100

    Added allegro libraries to GameEngine.h

--- src/GameEngine.h
+++ src/GameEngine.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <iostream>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <vector>
 #include "GraphicEngine.h"
 #include "Menu.h"
